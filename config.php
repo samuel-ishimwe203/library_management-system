35471aacad16f52930ed5144ca4b1d1ef9a1c4dc
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-14 23:54:29 +0200

    chore: dockerfile now is working

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 $host = "localhost";
 $dbname = "library_db";
-$user = "library_user";
-$pass = "YOUR_DATABASE_PASSWORD";
+$username = "library_user";
+$password = "YOUR_DB_PASSWORD";
 
 try {
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8",
-        $user,
-        $pass
+        $username,
+        $password
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed");
 }
-?>;
+?>
